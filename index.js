9ff9d717370f069e93d453c6103a415a2f251178
Waldon Julien <waldonjulienjr@gmail.com>
2025-08-10 16:02:47 -0500

    website does prompt

---
+++ index.js
@@ -0,0 +1,18 @@
+const flavors = [vanilla, strawberry, coffee];
+flavors.forEach((flavor) => console.log(flavor));
+
+const userInput = prompt(
+  "Hello, what flavor would you like? We have Vanilla, Strawberry , and Coffee."
+);
+
+let flavorSelected = userInput.split(",");
+console.log(flavorSelected);
+    for(userInput=)
+    if (userInput === flavors) {
+        console.log(userInput.split(","));
+};  
+   else {
+    console.log("Not a flavor we offer.");
+};
+
+
